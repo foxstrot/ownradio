47f863caa9eba7c10cfe555e4ab1b4644f725371
Andrey Balykov <abalykov@gmail.com>
2016-10-14 15:27:12 +0300

    Initial Code Commit

---
+++ src/OwnRadio.Client.Desktop/Model/Track.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace OwnRadio.Client.Desktop.Model
+{
+    /// <summary>
+    /// Class keeps info about single track
+    /// </summary>
+    public class Track
+    {
+        public enum Statuses
+        {
+            Skipped = -1,
+            Listened = 1,
+        }
+
+        /// <summary>
+        /// Track ID
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Track URI to obtain media content
+        /// </summary>
+        public Uri Uri { get; set; }
+
+        /// <summary>
+        /// Listen status
+        /// </summary>
+        public Statuses Status { get; set; } = Statuses.Skipped;
+
+        /// <summary>
+        /// DateTime then track was finished (by reaching the end or by skipping)
+        /// </summary>
+        public DateTime ListenEnd { get; set; }
+    }
+}
