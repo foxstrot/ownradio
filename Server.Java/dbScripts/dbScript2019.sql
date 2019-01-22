205be0a65b1e83fbacc84634f5277d18f36c903b
andreyNetVoxLab <andrey.golovin.sam@gmail.com>
2019-01-22 13:53:43 +0300

    Update dbScript2019.sql

--- Server.Java/dbScripts/dbScript2019.sql
+++ Server.Java/dbScripts/dbScript2019.sql
@@ -1,4 +1,54 @@
-CREATE OR REPLACE FUNCTION public.getuserrecommended(i_userid uuid)
+CREATE OR REPLACE FUNCTION public.getnexttrack_v2(i_deviceid uuid)
+ RETURNS TABLE(track character varying, method integer, useridrecommended character varying, txtrecommendedinfo character varying, timeexecute character varying)
+ LANGUAGE plpgsql
+AS $function$
+DECLARE
+		declare t timestamptz := clock_timestamp(); -- запоминаем начальное время выполнения процедуры
+		i_userid UUID; -- = i_deviceid; -- в дальнейшем заменить получением userid по deviceid
+		user_name character varying;
+	useridrecommended UUID;
+BEGIN
+	-- Добавляем устройство, если его еще не существует
+	PERFORM registerdevice(i_deviceid, 'New device');
+
+
+--если пользователь alexv_test то пускаем выполнение в getuserrecommended, иначе идем по старому
+
+select userid into i_userid from devices where recid = i_deviceid; --получаем id пользователя по id_device
+select recname into user_name from users where recid = i_userid; --получаем имя пользователя по его id
+
+
+if user_name = 'alexv2test' then
+--return QUERY select getuserrecommended(i_userid); --return getuserrecommended(i_userid)
+
+useridrecommended = cast('11111111-0000-0888-0000-000000000000' as UUID); --выдавать от пользователя alexv
+
+	RETURN QUERY SELECT
+					 nexttrack.track,
+					 nexttrack.methodid,
+					 nexttrack.useridrecommended,
+					 nexttrack.txtrecommendedinfo,
+					 CAST((clock_timestamp() - t ) AS CHARACTER VARYING) -- возвращаем время выполнения процедуры
+				 FROM getuserrecommended(i_userid, useridrecommended) AS nexttrack;
+
+return;
+end if;
+
+	-- Возвращаем trackid, конвертируя его в character varying, и methodid
+	RETURN QUERY SELECT
+					 CAST((nexttrack.track) AS CHARACTER VARYING),
+					 nexttrack.methodid,
+					 CAST((nexttrack.useridrecommended) AS CHARACTER VARYING),
+					 nexttrack.txtrecommendedinfo,
+					 CAST((clock_timestamp() - t ) AS CHARACTER VARYING) -- возвращаем время выполнения процедуры
+				 FROM getnexttrackid_v17(i_deviceid) AS nexttrack;
+END;
+$function$
+;
+
+
+
+CREATE OR REPLACE FUNCTION public.getuserrecommended(i_userid uuid, i_useridrecommended uuid)
  RETURNS TABLE(track character varying, methodid integer, useridrecommended character varying, txtrecommendedinfo character varying)
  LANGUAGE plpgsql
 AS $function$ 	
@@ -7,14 +57,24 @@ AS $function$
 --declare return_val UUID;
 declare --temp_track record;
 		 end_trackid UUID;
-		useridrecommended UUID;
+		i_deviceid UUID;
+	deviceid_recommended UUID;
+		--useridrecommended UUID;
 begin
+
+	--АВ устройства
+-- 57128f7c-307c-47d1-9e6e-e5f8d40a86d6
+-- ff87a125-71cd-4d4e-809d-a1216cc45bd1
 	
 		DROP TABLE IF EXISTS temp_track; 
 	CREATE TEMP TABLE temp_track(trackid character varying, meth integer, useridrecommended character varying, txtrecommeninfo character varying);
 	
 	end_trackid = cast('10000000-0000-0000-0000-000000000001' as UUID);
-useridrecommended = cast('11111111-0000-0888-0000-000000000000' as UUID);
+select recid into  i_deviceid from devices where userid = i_userid;
+select recid into  deviceid_recommended from devices where userid = i_useridrecommended;
+
+
+--useridrecommended = cast('11111111-0000-0888-0000-000000000000' as UUID);
 	
 	
 
@@ -30,13 +90,13 @@ useridrecommended = cast('11111111-0000-0888-0000-000000000000' as UUID);
 --		fetch first 1 rows only;
 	
 	insert into temp_track
-	select cast(tracks.recid as character varying) as trackid , 10 , '11111111-0000-0888-0000-000000000000' as useridrecommended, 'выдан по тестовой рекомендации'  as txtrecommendedinfo
+	select cast(tracks.recid as character varying) as trackid , 10 , i_useridrecommended, 'выдан по тестовой рекомендации'  as txtrecommendedinfo
  	 from tracks 
-	where deviceid = cast ('11111111-0000-0888-0000-000000000000' as UUID) 
+	where deviceid = deviceid_recommended 
 						and recid<> end_trackid 
 						and recid not in 
 								(
-									select trackid from downloadtracks where deviceid = cast ('11111111-0000-0888-0000-111111111111' as UUID)
+									select trackid from downloadtracks where deviceid = i_deviceid
 								)
 		order by random()
 		fetch first 1 rows only;
@@ -64,7 +124,7 @@ then
 				select
 					uuid_generate_v4(),
 					now(),
-					cast ('11111111-0000-0888-0000-111111111111' as UUID),
+					i_deviceid,
 					cast(trackid as UUID),
 					temp_track.meth,
 					txtrecommeninfo
@@ -75,9 +135,9 @@ then
 				
 else
 		return query
-		select cast('10000000-0000-0000-0000-000000000001' as character varying) as trackid , 
+		select cast(end_trackid as character varying) as trackid , 
 		10 as methodid ,
-		cast('11111111-0000-0888-0000-000000000000' as character varying) as useridrecommended, 
+		cast(i_useridrecommended as character varying), 
 		cast ('конец' as character varying ) as txtrecommendedinfo;
 end if;
 	
@@ -90,53 +150,3 @@ end if;
 end;
  $function$
 ;
-
-
-
-CREATE OR REPLACE FUNCTION public.getnexttrack_v2(i_deviceid uuid)
- RETURNS TABLE(track character varying, method integer, useridrecommended character varying, txtrecommendedinfo character varying, timeexecute character varying)
- LANGUAGE plpgsql
-AS $function$
-DECLARE
-		declare t timestamptz := clock_timestamp(); -- запоминаем начальное время выполнения процедуры
-		i_userid UUID = i_deviceid; -- в дальнейшем заменить получением userid по deviceid
-		declare user_name character varying;
-BEGIN
-	-- Добавляем устройство, если его еще не существует
-	PERFORM registerdevice(i_deviceid, 'New device');
-
-
---если пользователь alexv_test то пускаем выполнение в getuserrecommended, иначе идем по старому
-
---select userid into i_userid from devices where recid = i_deviceid; --получаем id пользователя по id_device
-select recname into user_name from users where recid = i_userid; --получаем имя пользователя по его id
-
-
-if user_name = 'alexv2test' then
---return QUERY select getuserrecommended(i_userid); --return getuserrecommended(i_userid)
-
-
-
-	RETURN QUERY SELECT
-					 nexttrack.track,
-					 nexttrack.methodid,
-					 nexttrack.useridrecommended,
-					 nexttrack.txtrecommendedinfo,
-					 CAST((clock_timestamp() - t ) AS CHARACTER VARYING) -- возвращаем время выполнения процедуры
-				 FROM getuserrecommended(i_userid) AS nexttrack;
-
-return;
-end if;
-
-	-- Возвращаем trackid, конвертируя его в character varying, и methodid
-	RETURN QUERY SELECT
-					 CAST((nexttrack.track) AS CHARACTER VARYING),
-					 nexttrack.methodid,
-					 CAST((nexttrack.useridrecommended) AS CHARACTER VARYING),
-					 nexttrack.txtrecommendedinfo,
-					 CAST((clock_timestamp() - t ) AS CHARACTER VARYING) -- возвращаем время выполнения процедуры
-				 FROM getnexttrackid_v17(i_deviceid) AS nexttrack;
-END;
-$function$
-;
-
